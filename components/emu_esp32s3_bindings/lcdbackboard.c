72a1685cfcd010897c07f95d41f0c336d297c0ca
Jeroen Domburg <git@j0h.nl>
2022-06-24 19:28:28 +0800

    Convert to 320x240 backbox LCD

--- components/emu_esp32s3_bindings/lcdbackboard.c
+++ components/emu_esp32s3_bindings/lcdbackboard.c
@@ -23,40 +23,22 @@ typedef struct {
 } lcd_init_cmd_t;
 
 DRAM_ATTR const lcd_init_cmd_t init_cmds[17]={
-	/* Memory Data Access Control, MX=MV=1, MY=ML=MH=0, RGB=0 */
-	{0x36, {(0<<5)|(0<<6)}, 1},
-	/* Interface Pixel Format, 16bits/pixel for RGB/MCU interface */
+	{0xC8, {0xFF, 0x93, 0x42}, 3},
+	{0xb6, {0x0a, 0xE0}, 2},
+	{0x36, {0x08}, 1},
 	{0x3A, {0x55}, 1},
-	/* Porch Setting */
-	{0xB2, {0x0c, 0x0c, 0x00, 0x33, 0x33}, 5},
-	/* Gate Control, Vgh=13.65V, Vgl=-10.43V */
-	{0xB7, {0x45}, 1},
-	/* VCOM Setting, VCOM=1.175V */
-	{0xBB, {0x2B}, 1},
-	/* LCM Control, XOR: BGR, MX, MH */
-	{0xC0, {0x2C}, 1},
-	/* VDV and VRH Command Enable, enable=1 */
-	{0xC2, {0x01, 0xff}, 2},
-	/* VRH Set, Vap=4.4+... */
-	{0xC3, {0x11}, 1},
-	/* VDV Set, VDV=0 */
-	{0xC4, {0x20}, 1},
-	/* Frame Rate Control, 60Hz, inversion=0 */
-	{0xC6, {0x0f}, 1},
-	/* Power Control 1, AVDD=6.8V, AVCL=-4.8V, VDDS=2.3V */
-	{0xD0, {0xA4, 0xA1}, 1},
-	/* Positive Voltage Gamma Control */
-	{0xE0, {0xD0, 0x00, 0x05, 0x0E, 0x15, 0x0D, 0x37, 0x43, 0x47, 0x09, 0x15, 0x12, 0x16, 0x19}, 14},
-	/* Negative Voltage Gamma Control */
-	{0xE1, {0xD0, 0x00, 0x05, 0x0D, 0x0C, 0x06, 0x2D, 0x44, 0x40, 0x0E, 0x1C, 0x18, 0x16, 0x19}, 14},
-	/* Sleep Out */
+	{0xC0, {0x13, 0x0E}, 2},
+	{0xC1, {0x02}, 1},
+	{0xC5, {0xCA}, 1},
+	{0xB1, {0x00, 0x1B}, 2},
+	{0xB4, {0x02}, 1},
+	{0xE0, {0x00, 0x02, 0x04, 0x00, 0x11, 0x08, 0x35, 0x79, 0x45, 0x07, 0x0D, 0x09, 0x16, 0x17, 0x0F}, 15},
+	{0xE1, {0x00, 0x28, 0x29, 0x02, 0x0F, 0x06, 0x3F, 0x25, 0x55, 0x06, 0x15, 0x0F, 0x38, 0x38, 0x0F}, 15},
+	{0x2A, {0x00, 0x00, 0x01, 0x3F}, 4},
+	{0x2B, {0x00, 0x00, 0x00, 0xEF}, 4},
 	{0x11, {0}, 0x80},
-#if !CONFIG_ESPPDP_HW_WROVER_KIT
-	{0x21, {0}, 0x80},
-#endif
-	/* Display On */
-	{0x29, {0}, 0x80},
-	{0, {0}, 0xff}
+	{0x29, {0}, 0},
+	{0x2C, {0}, 0},
 };
 
 
@@ -150,8 +132,8 @@ static void send_lines(spi_device_handle_t spi, int ypos, uint16_t *linedata) {
 	trans[0].tx_data[0]=0x2A;			//Column Address Set
 	trans[1].tx_data[0]=0;				//Start Col High
 	trans[1].tx_data[1]=0;				//Start Col Low
-	trans[1].tx_data[2]=(239)>>8;		//End Col High
-	trans[1].tx_data[3]=(239)&0xff;		//End Col Low
+	trans[1].tx_data[2]=(319)>>8;		//End Col High
+	trans[1].tx_data[3]=(319)&0xff;		//End Col Low
 	trans[2].tx_data[0]=0x2B;			//Page address set
 	trans[3].tx_data[0]=ypos>>8;		//Start page high
 	trans[3].tx_data[1]=ypos&0xff;		//start page low
@@ -159,7 +141,7 @@ static void send_lines(spi_device_handle_t spi, int ypos, uint16_t *linedata) {
 	trans[3].tx_data[3]=(ypos+PARALLEL_LINES)&0xff;	 //end page low
 	trans[4].tx_data[0]=0x2C;			//memory write
 	trans[5].tx_buffer=linedata;		//finally send the line data
-	trans[5].length=240*2*8*PARALLEL_LINES;			 //Data length, in bits
+	trans[5].length=320*2*8*PARALLEL_LINES;			 //Data length, in bits
 	trans[5].flags=0; //undo SPI_TRANS_USE_TXDATA flag
 
 	//Queue all transactions.
@@ -211,12 +193,12 @@ extern const uint8_t img_table4_rgb_start[] asm("_binary_table4_rgb_start");
 extern const uint8_t font[] asm("_binary_font_bin_start");
 
 static const images_t images[]={
-	{&img_pf_rgb_start[0], 240},
-	{&img_loading_rgb_start[0], 240},
-	{&img_table1_rgb_start[0], 120},
-	{&img_table2_rgb_start[0], 120},
-	{&img_table3_rgb_start[0], 120},
-	{&img_table4_rgb_start[0], 117}, //don't ask
+	{&img_pf_rgb_start[0], 320},
+	{&img_loading_rgb_start[0], 320},
+	{&img_table1_rgb_start[0], 160},
+	{&img_table2_rgb_start[0], 160},
+	{&img_table3_rgb_start[0], 160},
+	{&img_table4_rgb_start[0], 160}, //don't ask
 };
 
 uint8_t fade(int a, int b, int fade) {
@@ -235,7 +217,7 @@ void fade_bbimg(spi_device_handle_t spi, int cur_img, int next_img, uint16_t *li
 			uint16_t *p=line;
 			for (int y=0; y<PARALLEL_LINES; y++) {
 				if (y+yy<maxh) {
-					for (int x=0; x<240; x++) {
+					for (int x=0; x<320; x++) {
 						int r=fade(y+yy<images[cur_img].height?*imga++:0, y+yy<images[next_img].height?*imgb++:0,  f);
 						int g=fade(y+yy<images[cur_img].height?*imga++:0, y+yy<images[next_img].height?*imgb++:0,  f);
 						int b=fade(y+yy<images[cur_img].height?*imga++:0, y+yy<images[next_img].height?*imgb++:0,  f);
@@ -243,7 +225,7 @@ void fade_bbimg(spi_device_handle_t spi, int cur_img, int next_img, uint16_t *li
 						*p++=(pp>>8)|(pp<<8);
 					}
 				} else {
-					for (int x=0; x<240; x++) *p++=0;
+					for (int x=0; x<320; x++) *p++=0;
 				}
 			}
 			send_line_finish(spi);
@@ -257,17 +239,11 @@ void fade_bbimg(spi_device_handle_t spi, int cur_img, int next_img, uint16_t *li
 static void put_dmd(spi_device_handle_t spi, uint8_t *bf, uint32_t *pal, uint16_t *line_a, uint16_t *line_b) {
 	uint16_t *line=line_a;
 
-	//The DMD space in the framebuffer is 320x32 pixels. To simulate a DMD, every other line and column
-	//is not active (does not contain an actual DMD pixel) making the DMD itsefl 160x32 pixels. We want
-	//to show this in a 240x32 LCD space, so we need to compress 160 DMD pixels to 240 real pixels. We do
-	//this by subpixel rendering: for each 2 DMD pixels, we generate 3 LCD pixels as such:
-	//RGBRGBRGB <- 3 LCD pixels, with their RGB subpixels
-	//111x222xx <- Locations of (r,g) values of the DMD subpixels
 	for (int yy=0; yy<32; yy+=PARALLEL_LINES) {
 		uint16_t *p=line;
 		for (int y=0; y<PARALLEL_LINES; y++) {
 			uint8_t *pl=&bf[336*(yy+y)];
-			for (int x=0; x<240; x+=3) {
+			for (int x=0; x<320; x++) {
 				int r,g,b;
 				uint16_t pp;
 				r=(pal[pl[0]]>>16)&0xff;
@@ -275,17 +251,7 @@ static void put_dmd(spi_device_handle_t spi, uint8_t *bf, uint32_t *pal, uint16_
 				b=(pal[pl[0]]>>0)&0xff;
 				pp=((r>>3)<<11)|((g>>2)<<5)|((b>>3));
 				*p++=(pp>>8)|(pp<<8);
-				r=0;
-				g=(pal[pl[2]]>>8)&0xff;
-				b=(pal[pl[2]]>>0)&0xff;;
-				pp=((r>>3)<<11)|((g>>2)<<5)|((b>>3));
-				*p++=(pp>>8)|(pp<<8);
-				r=(pal[pl[2]]>>16)&0xff;
-				g=0;
-				b=0;
-				pp=((r>>3)<<11)|((g>>2)<<5)|((b>>3));
-				*p++=(pp>>8)|(pp<<8);
-				pl+=4;
+				pl++;
 			}
 		}
 		send_line_finish(spi);
@@ -297,8 +263,8 @@ static void put_dmd(spi_device_handle_t spi, uint8_t *bf, uint32_t *pal, uint16_
 void lcdbb_task(void *arg) {
 	spi_device_handle_t spi=(spi_device_handle_t)arg;
 	uint16_t *line_a, *line_b, *line;
-	line_a=calloc(PARALLEL_LINES, 240*2);
-	line_b=calloc(PARALLEL_LINES, 240*2);
+	line_a=calloc(PARALLEL_LINES, 320*2);
+	line_b=calloc(PARALLEL_LINES, 320*2);
 	assert(line_a);
 	assert(line_b);
 	line=line_a;
